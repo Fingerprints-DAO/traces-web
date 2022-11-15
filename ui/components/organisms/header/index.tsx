3626018d353722a4a1458d253c5c694bcef2802b
Thiago Lobão <thiago@blank.st>
2022-11-15 12:52:07 -0300

    [Header] Toggle wallet and show prints balance

--- ui/components/organisms/header/index.tsx
+++ ui/components/organisms/header/index.tsx
@@ -12,71 +12,38 @@ import { shortenAddress } from '@ui/utils/string'
 import useWalletConnection from '@ui/hooks/use-wallet-connect'
 
 const Header = () => {
-  const { WalletButton, walletIsConnected, provider, userAddress } =
-    useWalletConnection()
+    const { WalletButton, walletIsConnected, printsBalance, userAddress } = useWalletConnection()
 
-  //   console.log('provider', provider)
+    //   console.log('provider', provider)
 
-  return (
-    <Box
-      as="header"
-      paddingY="3"
-      background="gray.900"
-      position="sticky"
-      left={0}
-      top={0}
-      zIndex="sticky"
-    >
-      <Container
-        maxWidth="7xl"
-        display="flex"
-        justifyContent="space-between"
-        alignItems="center"
-      >
-        <Box display="flex" alignItems="center">
-          <Button
-            variant="unstyled"
-            display="block"
-            minWidth="unset"
-            height="unset"
-            padding="3px"
-            marginRight={[2, 4]}
-          >
-            <HamburgerIcon boxSize={18} display="block" />
-          </Button>
-          <Logo />
+    return (
+        <Box as="header" paddingY="3" background="gray.900" position="sticky" left={0} top={0} zIndex="sticky">
+            <Container maxWidth="7xl" display="flex" justifyContent="space-between" alignItems="center">
+                <Box display="flex" alignItems="center">
+                    <Button variant="unstyled" display="block" minWidth="unset" height="unset" padding="3px" marginRight={[2, 4]}>
+                        <HamburgerIcon boxSize={18} display="block" />
+                    </Button>
+                    <Logo />
+                </Box>
+                <Box display="flex" alignItems="center">
+                    {walletIsConnected && (
+                        <Box display="flex" alignItems="center" marginRight={[3, 6]}>
+                            <Box textAlign="right" marginRight={2}>
+                                <Text as="strong" color="gray.200" display="block" fontSize={['xs', 'sm']} fontWeight={600} marginBottom="-2px">
+                                    {printsBalance || 0} PRINTS
+                                </Text>
+                                <Text as="span" color="gray.400" fontSize={['xs', 'sm']} display="block">
+                                    {shortenAddress(userAddress)}
+                                </Text>
+                            </Box>
+                            <Avatar size={['sm', 'xm']} src="https://bit.ly/dan-abramov" />
+                        </Box>
+                    )}
+                    <WalletButton />
+                </Box>
+            </Container>
         </Box>
-        <Box display="flex" alignItems="center">
-          {walletIsConnected && (
-            <Box display="flex" alignItems="center" marginRight={[3, 6]}>
-              <Box textAlign="right" marginRight={2}>
-                <Text
-                  as="strong"
-                  color="gray.200"
-                  display="block"
-                  fontSize={['xs', 'sm']}
-                  fontWeight={600}
-                  marginBottom="-2px"
-                >
-                  1580 PRINTS
-                </Text>
-                <Text
-                  as="span"
-                  color="gray.400"
-                  fontSize={['xs', 'sm']}
-                  display="block"
-                >
-                  {shortenAddress(userAddress)}
-                </Text>
-              </Box>
-              <Avatar size={['sm', 'xm']} src="https://bit.ly/dan-abramov" />
-            </Box>
-          )}
-          <WalletButton />
-        </Box>
-      </Container>
-    </Box>
-  )
+    )
 }
 
 export default Header
--- ui/contexts/Web3Provider/index.tsx
+++ ui/contexts/Web3Provider/index.tsx
@@ -1,158 +1,138 @@
 import { useToast } from '@chakra-ui/react'
-import {
-  createContext,
-  PropsWithChildren,
-  useCallback,
-  useEffect,
-  useState,
-} from 'react'
+import { createContext, PropsWithChildren, useCallback, useEffect, useState } from 'react'
 import { Provider, ProviderType } from 'web3/provider'
-import { ContractExample } from 'web3/contracts/ContractExample'
-import { formatEther, formatUnits } from 'ethers/lib/utils'
+import PrintsContract from '@web3/contracts/PrintsContract'
+import { formatEther } from 'ethers/lib/utils'
+import { Contract } from 'ethers'
 
-const tokenContractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || ''
+const printsContractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || ''
 
-const providerClass = new Provider()
-const exampleContract = new ContractExample(providerClass.get())
+const providerInstance = new Provider()
+const printsContract = new PrintsContract(providerInstance.get())
 
 const DEFAULT_CONTEXT = {
-  provider: providerClass.get(),
-  /** @dev: only minter must be signed.
-   * The only feature we need be connected to client wallet is the mint.
-   * We need only get information on core and project data contracts, by now.
-   **/
-  exampleContractRead: exampleContract.get(),
-  walletIsConnected: false,
-  userAddress: '',
-  handleConnectWallet: () => {},
-  connectWalletHandler: () => {},
-  disconnectWallet: () => {},
+    provider: providerInstance.get(),
+    printsBalance: '0',
+    /** @dev: only minter must be signed.
+     * The only feature we need be connected to client wallet is the mint.
+     * We need only get information on core and project data contracts, by now.
+     **/
+    contractRead: printsContract.get(),
+    walletIsConnected: false,
+    userAddress: '',
+    handleConnectWallet: () => {},
+    connectWalletHandler: () => {},
+    disconnectWallet: () => {},
 }
 
 const Web3Context = createContext(DEFAULT_CONTEXT)
 
-export const Web3Provider: React.FC<PropsWithChildren> = ({
-  children,
-  ...rest
-}) => {
-  const toast = useToast()
+export const Web3Provider: React.FC<PropsWithChildren> = ({ children }) => {
+    const toast = useToast()
 
-  const [provider, setProvider] = useState<ProviderType>(
-    DEFAULT_CONTEXT.provider
-  )
-  const [userAddress, setUserAddress] = useState(DEFAULT_CONTEXT.userAddress)
-  const [walletIsConnected, setWalletIsConnected] = useState(
-    DEFAULT_CONTEXT.walletIsConnected
-  )
-  const [tradableContractSigned, setTradableContractSigned] = useState(
-    DEFAULT_CONTEXT.exampleContractRead
-  )
+    const [provider, setProvider] = useState<ProviderType>(DEFAULT_CONTEXT.provider)
+    const [userAddress, setUserAddress] = useState(DEFAULT_CONTEXT.userAddress)
+    const [walletIsConnected, setWalletIsConnected] = useState(DEFAULT_CONTEXT.walletIsConnected)
+    const [printsContractSigned, setPrintsContractSigned] = useState(DEFAULT_CONTEXT.contractRead)
+    const [printsBalance, setPrintsBalance] = useState(DEFAULT_CONTEXT.printsBalance)
 
-  console.log('tradableContractSigned', tradableContractSigned)
+    const checkBalance = useCallback(async (address: string, printsSigned: Contract) => {
+        const balance = await printsSigned.balanceOf(address)
+        const balanceStr = formatEther(balance)
 
-  const connectWalletHandler = useCallback(async () => {
-    if (walletIsConnected) return
+        setPrintsBalance(parseFloat(balanceStr).toFixed(2))
+    }, [])
 
-    await providerClass.connectWallet()
+    const connectWalletHandler = useCallback(async () => {
+        if (walletIsConnected) return
 
-    const connectedWallet = providerClass.getConnectedWallet()
+        const connectedWallet = await providerInstance.connectWallet()
 
-    if (!connectedWallet) return
+        if (!connectedWallet) return
 
-    const signer = connectedWallet.getSigner()
+        const signer = connectedWallet.getSigner()
 
-    if (!signer) return
+        if (!signer) return
 
-    const address = await signer.getAddress()
+        const address = await signer.getAddress()
 
-    if (!address) return
+        if (!address) return
 
-    setProvider(connectedWallet)
-    setUserAddress(address)
-    setWalletIsConnected(true)
+        const printsSigned = new PrintsContract(signer).get()
 
-    // const avatar = provider.getAvatar(address);
-    // const avatar = await connectedWallet.getAvatar()
-    // console.log('avatar', avatar)
+        setProvider(connectedWallet)
+        setUserAddress(address)
+        setWalletIsConnected(true)
+        setPrintsContractSigned(printsSigned)
 
-    // const balance = await signer.getBalance(
-    //   process.env.NEXT_PUBLIC_CONTRACT_ADDRESS || ''
-    // )
+        await checkBalance(address, printsSigned)
+    }, [walletIsConnected, checkBalance])
 
-    const contract = await tradableContractSigned.balanceOf(address)
-    console.log('contract', contract)
-    // const balance = await tradableContractSigned.balanceOf(address)
-    // console.log('balance', balance)
+    const handleConnectWallet = useCallback(async () => {
+        try {
+            await connectWalletHandler()
 
-    const balance2 = await provider.getBalance(tokenContractAddress)
-    // console.log('balance', formatEther(balance))
-    // console.log('balance', formatUnits(balance, 6))
-    console.log('balance2', formatUnits(balance2, 6))
-  }, [walletIsConnected, provider, tradableContractSigned])
+            toast({
+                title: 'Connect a wallet',
+                description: 'Wallet connected successfully',
+                status: 'success',
+                duration: 3000,
+                isClosable: true,
+            })
+        } catch (error) {
+            console.log('Error onConnectWallet', error)
 
-  const handleConnectWallet = useCallback(async () => {
-    try {
-      await connectWalletHandler()
+            toast({
+                title: 'Connect a wallet',
+                description: 'Error while connecting your wallet. Try again late.',
+                status: 'error',
+                duration: 9000,
+                isClosable: true,
+            })
+        }
+    }, [connectWalletHandler, toast])
 
-      toast({
-        title: 'Connect a wallet',
-        description: 'Wallet connected successfully',
-        status: 'success',
-        duration: 3000,
-        isClosable: true,
-      })
-    } catch (error) {
-      console.log('Error onConnectWallet', error)
+    const disconnectWallet = async () => {
+        setWalletIsConnected(false)
+        setProvider(DEFAULT_CONTEXT.provider)
+        setUserAddress(DEFAULT_CONTEXT.userAddress)
 
-      toast({
-        title: 'Connect a wallet',
-        description: 'Error while connecting your wallet. Try again late.',
-        status: 'error',
-        duration: 9000,
-        isClosable: true,
-      })
+        providerInstance.disconnectWallet()
     }
-  }, [connectWalletHandler, toast])
 
-  const disconnectWallet = async () => {
-    setWalletIsConnected(false)
-    setProvider(DEFAULT_CONTEXT.provider)
-    setUserAddress(DEFAULT_CONTEXT.userAddress)
+    const checkWalletConnection = useCallback(async () => {
+        if (typeof window.ethereum !== 'undefined') {
+            const addressList = await window.ethereum?.request({
+                method: 'eth_accounts',
+            })
 
-    providerClass.disconnectWallet()
-  }
+            if (addressList.length > 0) {
+                connectWalletHandler()
+            }
+        }
+    }, [connectWalletHandler])
 
-  const checkWalletConnection = useCallback(async () => {
-    if (typeof window.ethereum !== 'undefined') {
-      const addressList = await window.ethereum?.request({
-        method: 'eth_accounts',
-      })
+    useEffect(() => {
+        checkWalletConnection()
+    }, [checkWalletConnection])
 
-      if (addressList.length > 0) {
-        connectWalletHandler()
-      }
-    }
-  }, [connectWalletHandler])
-
-  useEffect(() => {
-    checkWalletConnection()
-  }, [checkWalletConnection])
-
-  return (
-    <Web3Context.Provider
-      value={{
-        ...DEFAULT_CONTEXT,
-        provider,
-        disconnectWallet,
-        walletIsConnected,
-        handleConnectWallet,
-        userAddress,
-        connectWalletHandler,
-      }}
-    >
-      {provider ? children : ''}
-    </Web3Context.Provider>
-  )
+    return (
+        <Web3Context.Provider
+            value={{
+                ...DEFAULT_CONTEXT,
+                provider,
+                printsBalance,
+                disconnectWallet,
+                walletIsConnected,
+                handleConnectWallet,
+                userAddress,
+                connectWalletHandler,
+            }}
+        >
+            {/* {provider ? children : ''} */}
+            {children}
+        </Web3Context.Provider>
+    )
 }
 
 export default Web3Context
