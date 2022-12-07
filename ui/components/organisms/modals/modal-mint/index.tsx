9e5b3c3edd248613b050d75b8ba5960522f199b2
Thiago Lobão <thiago@blank.st>
2022-12-06 22:42:35 -0300

    [WIP] Mint NFT

--- ui/components/organisms/modals/modal-mint/index.tsx
+++ ui/components/organisms/modals/modal-mint/index.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 
 // Dependencies
+import { BigNumber } from 'ethers'
 import { useAccount, useBalance } from 'wagmi'
 import { Modal, ModalBody, ModalContent, ModalOverlay } from '@chakra-ui/react'
 
@@ -10,7 +11,6 @@ import Actions from './actions'
 import ModalMintHeader from './header'
 import usePrintsRead from '@web3/contracts/prints/use-prints-read'
 import usePrintsApprove from '@web3/contracts/prints/use-prints-approve'
-import { BigNumber } from 'ethers'
 
 type ModalMintProps = {
   isOpen: boolean
@@ -23,20 +23,34 @@ const ModalMint = ({ isOpen, onClose }: ModalMintProps) => {
   const [amount, setAmount] = useState<BigNumber>()
 
   const { address } = useAccount()
+  const { allowance } = usePrintsRead()
+
+  const { write: approvePrints } = usePrintsApprove(amount)
   const { data: balance } = useBalance({ address, enabled: Boolean(address) && Boolean(printContractAddress), token: printContractAddress })
 
-  //   console.log('allowance', allowance?.toString())
+  const handleMint = useCallback(
+    (data: { amount: number }) => {
+      const allowanceUntilNow = allowance?.toNumber()
 
-  const handleMint = (data: { amount: string }) => {
-    setAmount(BigNumber.from(data.amount))
-  }
+      if (typeof allowanceUntilNow !== 'number') {
+        return
+      }
+
+      const balanceToApprove = data.amount - allowanceUntilNow
+
+      setAmount(BigNumber.from(balanceToApprove))
+
+      approvePrints?.()
+    },
+    [allowance, approvePrints]
+  )
 
   return (
     <Modal isOpen={isOpen} onClose={onClose} isCentered={true}>
       <ModalOverlay />
       <ModalContent background="gray.900" padding={[6, 12]} minW={['unset', 650]} maxW={['90%', '90%', '90%', 'md']}>
         <ModalMintHeader prints={Number(balance?.formatted)} />
-        <ModalBody padding={0}>{Boolean(amount) ? <Actions amount={amount!} onClose={onClose} /> : <Stake prints={Number(balance?.formatted)} onSubmit={handleMint} onClose={onClose} />}</ModalBody>
+        <ModalBody padding={0}>{!!amount ? <Actions amount={amount} onClose={onClose} /> : <Stake prints={Number(balance?.formatted)} onSubmit={handleMint} onClose={onClose} />}</ModalBody>
       </ModalContent>
     </Modal>
   )
--- web3/contracts/prints/use-prints-approve.ts
+++ web3/contracts/prints/use-prints-approve.ts
@@ -1,34 +1,20 @@
 // Dependencies
-import { useToast } from '@chakra-ui/react'
 import { BigNumber } from 'ethers'
-import { useAccount, useContractWrite, usePrepareContractWrite } from 'wagmi'
+import { useContractWrite, usePrepareContractWrite } from 'wagmi'
 
 // Helpers
 import PrintsContract from './contract'
 
 const usePrintsApprove = (amount?: BigNumber) => {
-  const toast = useToast()
-  const { address } = useAccount()
-
   const { config } = usePrepareContractWrite({
     address: process.env.NEXT_PUBLIC_PRINTS_CONTRACT_ADDRESS,
     abi: PrintsContract,
     functionName: 'approve',
-    enabled: Boolean(address) && Boolean(amount),
-    args: [address!, amount!],
+    enabled: !!amount,
+    args: [process.env.NEXT_PUBLIC_PRINTS_CONTRACT_ADDRESS || ('' as any), amount!],
   })
 
-  return useContractWrite({
-    ...config,
-    onSettled: (_, error) => {
-      if (!error) {
-        console.log('AQUI STTLED')
-      }
-    },
-    onError: () => {
-      toast({ title: 'Error', description: 'User reject transaction', status: 'error' })
-    },
-  })
+  return useContractWrite(config)
 }
 
 export default usePrintsApprove
