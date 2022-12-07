7851decd1f937494748805fd8e14fcf0c0c05e2a
arodundef <arod.mail+fp@proton.me>
2022-12-06 23:03:41 -0300

    fix(type): req.query.id to bignumber

--- pages/api/wnft/[id].ts
+++ pages/api/wnft/[id].ts
@@ -4,6 +4,7 @@ import { readContract } from '@wagmi/core'
 import { getChainId, handleToken } from '../helpers/_web3'
 import { Token, WNFTMetadata } from '../helpers/_types'
 import TracesContract from '@web3/contracts/traces/contract'
+import { BigNumber } from 'ethers/lib/ethers'
 
 // bitchcoin
 // address 0x5e86f887ff9676a58f25a6e057b7a6b8d65e1874
@@ -17,7 +18,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       abi: TracesContract,
       functionName: 'getToken',
       chainId: getChainId(),
-      args: [req.query.id],
+      args: [BigNumber.from(req.query.id)],
     })) as Token
   )
 
