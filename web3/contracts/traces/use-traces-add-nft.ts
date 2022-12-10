dcf9e004554ddc59b78aa86826379dddbb7dc692
arodundef <arod.mail+fp@proton.me>
2022-12-10 18:47:12 -0300

    fix(ui): from seconds to hours on dutch auction duration

--- web3/contracts/traces/use-traces-add-nft.ts
+++ web3/contracts/traces/use-traces-add-nft.ts
@@ -2,13 +2,14 @@ import { useContext } from 'react'
 
 // Dependencies
 import { useToast } from '@chakra-ui/react'
-import { useContractWrite, usePrepareContractWrite } from 'wagmi'
+import { Address, useContractWrite, usePrepareContractWrite } from 'wagmi'
 
 // Helpers
 import useTracesRead from './use-traces-read'
 import TracesContract from '@web3/contracts/traces/contract'
 import { ModalContext } from '@ui/contexts/Modal'
 import { AddNftPayload } from '@ui/components/organisms/modals/modal-add-nft'
+import { BigNumber } from 'ethers'
 
 const useTracesAddNft = (isSubmitted: boolean, payload: AddNftPayload) => {
   const toast = useToast()
@@ -20,12 +21,12 @@ const useTracesAddNft = (isSubmitted: boolean, payload: AddNftPayload) => {
     abi: TracesContract,
     functionName: 'addToken',
     args: [
-      process.env.NEXT_PUBLIC_ERC721_MOCK_CONTRACT_ADDRESS || ('' as any),
+      payload?.ogTokenAddress as Address,
       payload?.ogTokenId as any,
       payload?.minStake as any,
       payload?.minHoldPeriod as any,
       payload?.dutchMultiplier as any,
-      payload?.dutchAuctionDuration as any,
+      BigNumber.from(payload?.dutchAuctionDuration).mul(60),
     ],
     enabled: isSubmitted && !!isEditor && !!payload,
   })
