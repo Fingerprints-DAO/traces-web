fcce73300bb584272f8b329530abfd85953a5890
arodundef <arod.mail+fp@proton.me>
2023-01-05 22:55:05 -0300

    fix(next): add full reservoir url

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['picsum.photos', 'reservoir.tools'],
+    domains: ['picsum.photos', 'reservoir.tools', 'api-goerli.reservoir.tools'],
   },
 }
 
