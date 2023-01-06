08c029ce9ffad58ba57be665d571754f1869206f
Thiago Lobão <thiago@blank.st>
2023-01-05 23:08:45 -0300

    [Administrators] List all admins

--- ui/components/organisms/modals/modal-administrators/index.tsx
+++ ui/components/organisms/modals/modal-administrators/index.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 
 // Dependencies
+import { useQuery } from 'react-query'
 import { Box, Button, Heading, Modal, ModalBody, ModalContent, ModalOverlay, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
 
 // Helpers
 import { ModalProps } from '@ui/contexts/Modal'
+import { getBuiltGraphSDK } from '.graphclient'
+import { shortenAddress } from '@ui/utils/string'
+
+const sdk = getBuiltGraphSDK()
 
 const ModalAdministrators = ({ isOpen, onClose }: ModalProps) => {
+  const { data: administrators, isLoading, isError } = useQuery({ queryKey: 'GetAdministrators', queryFn: () => sdk.GetAdministrators() })
+
+  const isEmpty = useMemo(() => !administrators?.admins.concat(administrators.editors).length, [administrators])
+
   return (
     <Modal isOpen={isOpen} onClose={onClose} isCentered={true}>
       <ModalOverlay />
@@ -17,7 +26,7 @@ const ModalAdministrators = ({ isOpen, onClose }: ModalProps) => {
           </Heading>
         </Box>
         <ModalBody padding={0}>
-          <TableContainer>
+          <TableContainer overflowX={['auto', 'visible']} overflowY={['auto', 'visible']}>
             <Table colorScheme="whiteAlpha" variant="simple" border={1} borderStyle="solid" borderColor="gray.700">
               <Thead bg="gray.700">
                 <Tr>
@@ -31,32 +40,52 @@ const ModalAdministrators = ({ isOpen, onClose }: ModalProps) => {
                 </Tr>
               </Thead>
               <Tbody>
-                <Tr>
-                  <Td color="gray.300" fontSize="sm" borderBottom={1} borderBottomStyle="solid" borderBottomColor="gray.700">
-                    Admin
-                  </Td>
-                  <Td color="gray.300" fontSize="sm" borderBottom={1} borderBottomStyle="solid" borderBottomColor="gray.700">
-                    0x0dAb8FDeFfc501b...15952Dcd7bf116D5B506
-                  </Td>
-                  <Td isNumeric borderBottom={1} borderBottomStyle="solid" borderBottomColor="gray.700">
-                    <Button colorScheme="blue" variant="link">
-                      Delete
-                    </Button>
-                  </Td>
-                </Tr>
-                <Tr>
-                  <Td color="gray.300" fontSize="sm" borderBottom={1} borderBottomStyle="solid" borderBottomColor="gray.700">
-                    Admin
-                  </Td>
-                  <Td color="gray.300" fontSize="sm" borderBottom={1} borderBottomStyle="solid" borderBottomColor="gray.700">
-                    0x0dAb8FDeFfc501b...15952Dcd7bf116D5B506
-                  </Td>
-                  <Td isNumeric borderBottom={1} borderBottomStyle="solid" borderBottomColor="gray.700">
-                    <Button colorScheme="blue" variant="link">
-                      Delete
-                    </Button>
-                  </Td>
-                </Tr>
+                {isLoading ? (
+                  <Tr>
+                    <Td colSpan={3}>Loading...</Td>
+                  </Tr>
+                ) : isError || isEmpty ? (
+                  <Tr>
+                    <Td colSpan={3}>No administrators found</Td>
+                  </Tr>
+                ) : (
+                  <>
+                    {administrators?.admins.map((item) => {
+                      return (
+                        <Tr key={item.id}>
+                          <Td color="gray.300" fontSize="sm" borderBottom={1} borderBottomStyle="solid" borderBottomColor="gray.700">
+                            Admin
+                          </Td>
+                          <Td color="gray.300" fontSize="sm" borderBottom={1} borderBottomStyle="solid" borderBottomColor="gray.700">
+                            {shortenAddress(item.id, 17, 20)}
+                          </Td>
+                          <Td isNumeric borderBottom={1} borderBottomStyle="solid" borderBottomColor="gray.700">
+                            <Button colorScheme="blue" variant="link">
+                              Delete
+                            </Button>
+                          </Td>
+                        </Tr>
+                      )
+                    })}
+                    {administrators?.editors.map((item) => {
+                      return (
+                        <Tr key={item.id}>
+                          <Td color="gray.300" fontSize="sm" borderBottom={1} borderBottomStyle="solid" borderBottomColor="gray.700">
+                            Editors
+                          </Td>
+                          <Td color="gray.300" fontSize="sm" borderBottom={1} borderBottomStyle="solid" borderBottomColor="gray.700">
+                            {shortenAddress(item.id, 17, 20)}
+                          </Td>
+                          <Td isNumeric borderBottom={1} borderBottomStyle="solid" borderBottomColor="gray.700">
+                            <Button colorScheme="blue" variant="link">
+                              Delete
+                            </Button>
+                          </Td>
+                        </Tr>
+                      )
+                    })}
+                  </>
+                )}
               </Tbody>
             </Table>
           </TableContainer>
