be5fe4a0b8e350e32fc3a5aa4bd4fa932a32c147
arodundef <arod.mail+fp@proton.me>
2023-01-15 23:41:56 -0300

    fix(roles): admin can add any role

--- web3/contracts/traces/use-traces-add-role.ts
+++ web3/contracts/traces/use-traces-add-role.ts
@@ -3,17 +3,11 @@ import useTraces from './use-traces'
 import { useMutation } from 'react-query'
 import { AddRolePayload } from '@ui/components/organisms/modals/modal-add-role'
 
-const useTracesAddRole = (isAdmin?: boolean, isEditor?: boolean, adminRole?: Address, onWaitSuccess?: () => void) => {
+const useTracesAddRole = (isAdmin?: boolean, adminRole?: Address) => {
   const traces = useTraces()
 
   const request = async ({ account, role }: AddRolePayload) => {
-    if (!isAdmin || !isEditor) {
-      throw new Error('User does not have permission')
-    }
-
-    const payloadRoleIsAdmin = role === adminRole
-
-    if (payloadRoleIsAdmin && !isAdmin) {
+    if (!isAdmin) {
       throw new Error('User does not have permission')
     }
 
